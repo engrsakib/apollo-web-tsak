0b40be9237dc809b5d81fedc4ebf4821322b0a40
engrsakib <snazmussakib01@gmail.com>
2025-05-04 12:24:36 +0600

    task 3 for 040525

--- mission 1/day 2/src/pactrice.js
+++ mission 1/day 2/src/pactrice.js
@@ -1,3 +1,8 @@
 //Task 1
 var prac1 = "Hello World, I will complete this course successfully and become a Next level Web Developer!";
-console.log(prac1);
+console.log("task 1", prac1);
+// task 2
+var task2 = function (name, age, role) {
+    return "Hello ".concat(name, ", you are ").concat(age, " years old and your role is ").concat(role, ".");
+};
+console.log("task 2", task2("Md. Nazmus Sakib", 25, "admin"));
--- mission 1/day 2/src/pactrice.ts
+++ mission 1/day 2/src/pactrice.ts
@@ -1,5 +1,11 @@
 //Task 1
 let prac1 : string = "Hello World, I will complete this course successfully and become a Next level Web Developer!"
-console.log(prac1)
+console.log("task 1", prac1)
 
 // task 2
+const task2 = (name: string, age: number, role: "admin" | "user" | "guest") => {
+    return `Hello ${name}, you are ${age} years old and your role is ${role}.`;
+}
+console.log("task 2", task2("Md. Nazmus Sakib", 25, "admin"));
+
+// task 3
