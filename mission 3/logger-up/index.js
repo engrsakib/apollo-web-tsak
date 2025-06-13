efa1374dda0e73d3c4ab155ebb6fed1a0c0bc97a
engrsakib <snazmussakib01@gmail.com>
2025-06-13 08:20:08 +0600

    update

--- mission 3/logger-up/index.js
+++ mission 3/logger-up/index.js
@@ -1,1 +1,7 @@
 console.log(process.argv)
+let a = 10;
+
+let b = 20;
+let c = (a, b) => a + b;
+console.log(`The sum of ${a} and ${b} is ${c}`);
+console.log(`The sum of ${a} and ${b} is ${c(a, b)}`);
